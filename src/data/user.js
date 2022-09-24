1fa6fd66747b84574c70c18226d91cb0a16bd046
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-24 11:30:39 +0300

    assignment Completed

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Alex",
+  city: "Kenya",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/AlexwKimotho",
+    linkedin: "https://www.linkedin.com/in/alex-wachira-kimotho-a92a31227?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMU7AmjbSS5SP2qQPEludNQ%3D%3D",
   },
   projects: [
     {
